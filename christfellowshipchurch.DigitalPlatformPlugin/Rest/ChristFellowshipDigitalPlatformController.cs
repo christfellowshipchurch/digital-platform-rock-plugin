f9e05afb3760807d9c2f09ba8dc287ed02872e2d
christfellowshipdev <44588555+christfellowshipdev@users.noreply.github.com>
2022-03-23 12:23:22 -0400

    check in

---
+++ christfellowshipchurch.DigitalPlatformPlugin/Rest/ChristFellowshipDigitalPlatformController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.OData;
+
+using Rock;
+using Rock.Data;
+using Rock.Model;
+using Rock.Rest;
+using Rock.Rest.Filters;
+using Rock.Web.Cache;
+
+namespace christfellowshipchurch.DigitalPlatformPlugin.Rest
+{
+    public class ChristFellowshipDigitalPlatformController : ApiControllerBase
+    {
+
+        [HttpGet]
+        [EnableQuery]
+        [Authenticate, Secured]
+        [System.Web.Http.Route("api/ChristFellowshipDigitalPlatform/test")]
+        public String Test( string input )
+        {
+            return input;
+        }
+
+    }
+}
