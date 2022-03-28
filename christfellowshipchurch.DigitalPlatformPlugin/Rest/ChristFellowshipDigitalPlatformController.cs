57cc10032a0f6daa218998646a58fd13e899a70c
christfellowshipdev <44588555+christfellowshipdev@users.noreply.github.com>
2022-03-28 15:50:41 -0400

    Update ChristFellowshipDigitalPlatformController.cs

--- christfellowshipchurch.DigitalPlatformPlugin/Rest/ChristFellowshipDigitalPlatformController.cs
+++ christfellowshipchurch.DigitalPlatformPlugin/Rest/ChristFellowshipDigitalPlatformController.cs
@@ -14,23 +14,57 @@ using Rock.Model;
 using Rock.Rest;
 using Rock.Rest.Filters;
 using Rock.Web.Cache;
+using Ical.Net.DataTypes;
+
+public struct ScheduleOccurrence
+{
+    public ScheduleOccurrence(DateTime start, DateTime end) : this()
+    {
+        Start = start;
+        End = end;
+    }
+
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+}
 
 namespace christfellowshipchurch.DigitalPlatformPlugin.Rest
 {
     public class ChristFellowshipDigitalPlatformController : ApiControllerBase
     {
+
         /// <summary>
-        /// A test method to check and see if this thing works
+        /// Parses a schedule into upcoming DateTimes. Optionally pass through a specific start and end date for retreiving all dates within the given range.
         /// </summary>
-        /// <param name="input"></param>
+        /// <param name="scheduleId"></param>
+        /// <param name="startTime"></param>
+        /// <param name="endTime"></param>
         /// <returns></returns>
         [HttpGet]
         [EnableQuery]
         [Authenticate, Secured]
-        [System.Web.Http.Route("api/ChristFellowshipDigitalPlatform/test")]
-        public string Test( string input )
+        [System.Web.Http.Route("api/ChristFellowshipDigitalPlatform/Schedule/ParseDates/{scheduleId}")]
+        public ScheduleOccurrence[] ScheduleDates(int scheduleId, DateTime? startTime = null, DateTime? endTime = null)
         {
-            return input;
+            var rockContext = new RockContext();
+            rockContext.Configuration.ProxyCreationEnabled = false;
+            ScheduleService scheduleService = new ScheduleService(rockContext);
+            Schedule schedule = scheduleService.Get(scheduleId);
+
+            DateTime start = DateTime.Now;
+            if (startTime != null)
+            {
+                start = (DateTime)startTime;
+            }
+
+            IList<Occurrence> occurrences = schedule.GetICalOccurrences(start, endTime);
+
+
+            return occurrences
+                .Select(o => new ScheduleOccurrence(
+                    start: o.Period.StartTime.Value,
+                    end: o.Period.EndTime.Value)
+                ).ToArray();
         }
 
     }
